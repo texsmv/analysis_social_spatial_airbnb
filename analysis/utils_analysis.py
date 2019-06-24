741183dcd3938c402f4c3dc79095ffd283870c6f
Alexis <texs.mv@gmail.com>
2019-06-24 09:13:12 -0500

    reduccion de dimensionalidad
    gg con las no lineales

--- analysis/utils_analysis.py
+++ analysis/utils_analysis.py
@@ -1,3 +1,6 @@
+from sklearn import preprocessing
+import numpy as np
+import pandas as pd
 
 # cast percentage to int
 def percentage_2_int(x):
@@ -13,3 +16,10 @@ def get_amenitie(x):
     t = t.replace('"', '')
     t = t.split(',')
     return t
+
+def normalize_dataframe(df):
+    x = df.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled)
+    return df
