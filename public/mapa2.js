7ce3355fd96a3d2264a3d3a3f14f78e2e3e132d7
Luis <luisbch9@gmail.commmmmmm>
2019-07-18 10:13:43 -0500

    fin

---
+++ public/mapa2.js
@@ -0,0 +1,88 @@
+
+$.get('paris.geojson', function (parisJson) {
+    
+    echarts.registerMap('paris', parisJson);
+    myChartMapaTres.setOption(
+        
+        {geo: {
+        map: 'paris',
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: function (params, ticket, callback) {
+                return "Cluster: " + params.data[2]
+            }
+        },
+        })   
+});
+
+
+
+$.get('data.json',function(datajson){
+    
+    myChartMapaTres.setOption({
+        
+        visualMap: [
+        {
+            min: 0,
+            max: datajson.reduce( (prev, current) => (prev > current.price ) ? prev : current.price ),
+            splitNumber: 6,
+            // precision: 2,
+            // categories : [ "0" , "1", "2" ],
+            color: ['#d94e5d','#eac736','#50a3ba'],
+            textStyle: {
+                color: '#000'
+            }
+        }
+        // {
+        //     // min: 0,
+        //     // max: 3,
+        //     // splitNumber: 3,
+        //     precision: 2,
+        //     categories : [ "0" , "1", "2" ],
+        //     align:'right',
+        //     color: ['#d94e5d','#eac736','#50a3ba'],
+        //     textStyle: {
+        //         color: '#000'
+        //     }
+        // },
+    
+    ],
+
+        series: [
+        {
+        type: 'heatmap',
+        coordinateSystem: 'geo',
+        data: datajson.map( dato => [dato.longitude,dato.latitude, dato.price ]),
+        // data: datajson.map( dato => [dato.longitude,dato.latitude,Math.floor(dato.label).toString() ]),
+        // symbolSize: 3,
+        blursize: 5,
+        itemStyle: {
+            emphasis: {
+                borderColor: '#000',
+                borderWidth: 1
+            }
+        }
+    }
+
+        // {
+        //     type: 'scatter',
+        //     coordinateSystem: 'geo',
+        //     // data: datajson.map( dato => [dato.longitude,dato.latitude, dato.price ]),
+        //     data: datajson.map( dato => [dato.longitude,dato.latitude,Math.floor(dato.label).toString() ]),
+        //     symbolSize: 3,
+           
+        //     itemStyle: {
+        //         emphasis: {
+        //             borderColor: '#000',
+        //             borderWidth: 1
+        //         }
+        //     }
+        // }
+    
+    
+    ]
+    })
+
+  
+});
