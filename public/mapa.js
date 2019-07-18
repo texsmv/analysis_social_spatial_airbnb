91a854d8df167422703df10b8c3134bca7cd5c29
Luis <luisbch9@gmail.commmmmmm>
2019-07-18 09:43:57 -0500

    ok

--- public/mapa.js
+++ public/mapa.js
@@ -22,48 +22,63 @@ $.get('data.json',function(datajson){
     
     myChartMapa.setOption({
         
-        visualMap: {
-            min: 0,
-            max: datajson.reduce( (prev, current) => (prev > current.price ) ? prev : current.price ),
-            splitNumber: 6,
-            // precision: 2,
-            // categories : [ "0" , "1", "2" ],
+        visualMap: [
+        // {
+        //     min: 0,
+        //     max: datajson.reduce( (prev, current) => (prev > current.price ) ? prev : current.price ),
+        //     splitNumber: 6,
+        //     // precision: 2,
+        //     // categories : [ "0" , "1", "2" ],
+        //     color: ['#d94e5d','#eac736','#50a3ba'],
+        //     textStyle: {
+        //         color: '#000'
+        //     }
+        // },
+        {
+            // min: 0,
+            // max: 3,
+            // splitNumber: 3,
+            precision: 2,
+            categories : [ "0" , "1", "2" ],
+            align:'right',
             color: ['#d94e5d','#eac736','#50a3ba'],
             textStyle: {
                 color: '#000'
             }
         },
+    
+    ],
 
         series: [
-            {
-            type: 'heatmap',
+    //     {
+    //     type: 'heatmap',
+    //     coordinateSystem: 'geo',
+    //     data: datajson.map( dato => [dato.longitude,dato.latitude, dato.price ]),
+    //     // data: datajson.map( dato => [dato.longitude,dato.latitude,Math.floor(dato.label).toString() ]),
+    //     // symbolSize: 3,
+    //     blursize: 5,
+    //     itemStyle: {
+    //         emphasis: {
+    //             borderColor: '#000',
+    //             borderWidth: 1
+    //         }
+    //     }
+    // },
+
+        {
+            type: 'scatter',
             coordinateSystem: 'geo',
-            data: datajson.map( dato => [dato.longitude,dato.latitude, dato.price ]),
-            // data: datajson.map( dato => [dato.longitude,dato.latitude,Math.floor(dato.label).toString() ]),
-            // symbolSize: 3,
-            blursize: 5,
+            // data: datajson.map( dato => [dato.longitude,dato.latitude, dato.price ]),
+            data: datajson.map( dato => [dato.longitude,dato.latitude,Math.floor(dato.label).toString() ]),
+            symbolSize: 3,
+           
             itemStyle: {
                 emphasis: {
                     borderColor: '#000',
                     borderWidth: 1
                 }
             }
-        },
-
-        // {
-        //     type: 'scatter',
-        //     coordinateSystem: 'geo',
-        //     data: datajson.map( dato => [dato.longitude,dato.latitude, dato.price ]),
-        //     // data: datajson.map( dato => [dato.longitude,dato.latitude,Math.floor(dato.label).toString() ]),
-        //     symbolSize: 3,
-           
-        //     itemStyle: {
-        //         emphasis: {
-        //             borderColor: '#000',
-        //             borderWidth: 1
-        //         }
-        //     }
-        // }
+        }
     
     
     ]
